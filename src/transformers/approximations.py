b02c03d25a1b740b1735fcf36a63546e5c7d3eaa
Leo de Castro <ldec@mit.edu>
2024-05-16 17:52:23 -0400

    candidate approximations for square root and max

--- src/transformers/approximations.py
+++ src/transformers/approximations.py
@@ -65,34 +65,75 @@ def approx_less_than(x, t):
 
 # SOFTMAX
 
+def approx_sqrt(x, d):
+    assert(torch.all(0 <= x))
+    assert(torch.all(x <= 1))
+    a = x
+    b = x-1
+    for _ in range(d):
+        a *= 1 - b/2
+        b = pow(b, 2) * (b - 3)/4
+    return a
 
-def approx_exp(x):
-    PARAM_R = 6
-    output = (1 + (x/(2**PARAM_R)))**(2**PARAM_R)
+
+def approx_max(x_vec, d):
+    ## 1) compute mean
+    ## 2) subtract x and square
+    ## 3) compute mean
+    ## This gives variance. Shift of 4*variance will likely be very close to the max
+
+    print("beginning max with input ", x_vec)
+
+    mean = sum(x_vec)/len(x_vec)
+    x_sub = [pow(x - mean, 2) for x in x_vec]
+    variance = sum(x_sub)/len(x_sub)
+
+    SCALE = 10000
+
+    # return 4*sqrt(variance)
+    return 5*approx_sqrt(variance/SCALE, d) * sqrt(SCALE)
+
+def approx_exp(x, r = 6):
+    # return torch.exp(x)
+    # PARAM_R = 6
+    output = pow(1 + x/pow(2, r), pow(2, r))
     # Set output of -inf entries to 0
     # -inf is sometimes (always?) equal to -3.4028e+38
-    output[x <= -3.4028e+38] = 0
+    output[x <= -3.4028e+37] = 0
     return output
 
 def approx_inv(x, d=5):
+    # assert(0 < x)
+    assert(torch.all(x > 0))
+    # assert(torch.all(x < 2))
+    if (not torch.all(x < 2)):
+        print(x)
+        assert(False)
     a = 2 - x
     b = 1 - x 
     for _ in range(d):
         b = pow(b, 2)
-        a += 1 + b 
+        a *= 1 + b 
     return a
 
 def ref_softmax(x, dim=None):
     maxes = torch.max(x, dim, keepdim=True)[0]
-
     x_exp = torch.exp(x-maxes)
     x_exp_sum = torch.sum(x_exp, dim, keepdim=True)
     return x_exp/x_exp_sum
 
 
 def approx_softmax(x, dim=None):
+    # x[x <= -3.4028e+37] = -1e6
     # print(f"max:\n{x.max()}")
     maxes = torch.max(x, dim, keepdim=True)[0]
+    
+    # maxes = approx_max(x, 6)
+    # correct_max = torch.max(x, dim, keepdim=True)[0]
+    # print(maxes, correct_max)
+    # assert(maxes == correct_max)
+    # # maxes = torch.empty(x.dim()).fill_(approx_max(x, 6))
+    
 
     # # For debugging
     # if not torch.all(x-maxes <= 0):
@@ -103,9 +144,14 @@ def approx_softmax(x, dim=None):
     #     print("-----------------------------------------\n")
     #     raise RuntimeError
 
-    x_exp = approx_exp(x-maxes)
+    x_exp = approx_exp(x-maxes)   ## all exponents are < 0
+    # x_exp = approx_exp(-1*x)   ## all exponents are < 0
     # x_exp = torch.exp(x-maxes)
 
+    SCALE = 1e10
+
     x_exp_sum = torch.sum(x_exp, dim, keepdim=True)
-    # return x_exp/x_exp_sum
-    return x_exp * approx_inv(x_exp_sum)
+    # print(x_exp_sum)
+    # print(x_exp_sum, 1/x_exp_sum, approx_inv(x_exp_sum / SCALE, d=10) / SCALE)
+    return x_exp/x_exp_sum
+    # return x_exp * approx_inv(x_exp_sum / SCALE, d=6) / SCALE
