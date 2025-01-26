1897cdbe485db02a1647c9d22384db0978bde22d
adyaagrawal <adyaagrawal04@gmail.com>
2025-01-27 01:08:04 +0530

    trying wsc- replace maxes with full tensor (max)

--- src/transformers/approximations.py
+++ src/transformers/approximations.py
@@ -519,7 +519,7 @@ def approx_softmax_without_max_replacement(x, layer_id, dim=None): #FINAL FUNCTI
 def approx_softmax(x, layer_id, dim=None): #FINAL FUNCTION WITH ALL APPROXIMATIONS
     # correct_maxes = torch.max(x, dim, keepdim=True)[0]
     # assert(correct_maxes == maxes)
-    maxes = torch.full((1, x.shape[1], x.shape[2], 1), mean_constants[layer_id])
+    maxes = torch.full((1, x.shape[1], x.shape[2], 1), max_constants[layer_id])
     if torch.cuda.is_available():
         maxes = maxes.to('cuda')
     EXP_ITERATIONS = 7
