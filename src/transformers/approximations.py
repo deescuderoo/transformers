7c72c6e479743a29be270ec6791282013586ea9f
adyaagrawal <adyaagrawal04@gmail.com>
2025-01-29 14:20:41 +0530

    running gpt2-medium - replace maxes for piqa, clean code

--- src/transformers/approximations.py
+++ src/transformers/approximations.py
@@ -427,7 +427,7 @@ def approx_softmax(x, layer_id, model, dim=None): #FINAL FUNCTION WITH ALL APPRO
     # Access the tensor dictionary dynamically
     tensor_dict = globals().get(tensor_name)
     maxes = tensor_dict[layer_id]
-
+    print(maxes.shape)
     maxes = maxes[:, :, :x.shape[2], :]
     if torch.cuda.is_available():
              maxes = maxes.to('cuda')
