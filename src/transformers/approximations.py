fe904a0b11adaa63490551f00f1c63e5e389bdc1
adyaagrawal <adyaagrawal04@gmail.com>
2025-01-30 17:11:12 +0530

    running gpt2 race - store

--- src/transformers/approximations.py
+++ src/transformers/approximations.py
@@ -292,7 +292,7 @@ def approx_softmax_store_in_file(x, layer_id, model, dim=None):
     # Get max values along the specified dimension
     maxes = torch.max(x, dim, keepdim=True)[0]  # Shape: [1, 12, W, 1]
     maxes_reshaped = maxes.squeeze(0).squeeze(-1).cpu().numpy()  # Shape: [12, W]
-    MAX_VALUES_FILE = f"layer_max_tensors_{model}_arc.txt"
+    MAX_VALUES_FILE = f"layer_max_tensors_{model}_race.txt"
     # Check if the file exists and if the layer's data is already present
     if not os.path.exists(MAX_VALUES_FILE):
         with open(MAX_VALUES_FILE, 'w') as file:
@@ -364,7 +364,7 @@ def approx_softmax_store_in_file(x, layer_id, model, dim=None):
 
     # norm: divide by length so that quotient is <1 (denominator
     # becomes the mean)
-    G_ITERATIONS = 7
+    G_ITERATIONS = 14
     if torch.cuda.is_available():
         normalizer = normalizer.to('cuda')
         # print(f"Device: {normalizer.device}")
