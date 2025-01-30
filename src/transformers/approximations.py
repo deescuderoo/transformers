09ee75941b9efae58bc3a3f0d1b631237fcda75b
adyaagrawal <adyaagrawal04@gmail.com>
2025-01-30 16:46:00 +0530

    running gpt2 arc easy - replace with piqa, increase iterations softmax

--- src/transformers/approximations.py
+++ src/transformers/approximations.py
@@ -458,7 +458,7 @@ def approx_softmax(x, layer_id, model, dim=None): #FINAL FUNCTION WITH ALL APPRO
 
     # norm: divide by length so that quotient is <1 (denominator
     # becomes the mean)
-    G_ITERATIONS = 12 #check
+    G_ITERATIONS = 11 #check
     if torch.cuda.is_available():
         normalizer = normalizer.to('cuda')
         # print(f"Device: {normalizer.device}")
