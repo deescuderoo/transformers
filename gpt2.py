b89baf1b444b92c6ed45fcb6cadc20c6ce0a205e
adyaagrawal <adyaagrawal04@gmail.com>
2025-03-28 20:01:20 +0530

    running 3 anli tests for gpt small with piqa tensor

--- gpt2.py
+++ gpt2.py
@@ -298,7 +298,9 @@ tasks = [
     # "truthfulqa_gen" -- too time taking
     # "openbookqa"
     # "nq_open" -- too time taking
-    "anli_r1"
+    "anli_r1",
+    "anli_r2",
+    "anli_r3"
         ]
 batch_size = 8
 task_manager = lm_eval.tasks.TaskManager()
