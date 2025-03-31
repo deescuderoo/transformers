ada7249ffd225e13677448a4b5ff9cdf30ffa00b
adyaagrawal <adyaagrawal04@gmail.com>
2025-04-01 05:15:15 +0530

    running social for gpt large with piqa tensor

--- gpt2.py
+++ gpt2.py
@@ -31,7 +31,7 @@ from transformers import GPT2LMHeadModelNew
 
 configuration = GPT2Config()
 
-gpt2 = "gpt2-medium" # "gpt2-xl" "gpt2" "gpt-large" "gpt2-medium"
+gpt2 = "gpt2-large" # "gpt2-xl" "gpt2" "gpt-large" "gpt2-medium"
 
 # This is the default GPT2 model from HF
 std_model = GPT2LMHeadModel.from_pretrained(gpt2)
@@ -278,7 +278,7 @@ from lm_eval.models.huggingface import HFLM
 tasks = [
     # "lambada_openai",
     # "hellaswag"
-    "arc_easy",
+    # "arc_easy",
     # "wikitext", -- not accurate
     # "glue",
     # "storycloze", -- needs custom dataset download
@@ -288,11 +288,11 @@ tasks = [
     # "piqa"
     # "race"
     # "triviaqa",
-    "social_iqa",
+    "social_iqa"
     # "squad_completion" --too long for gpt small
-    "mnli",
+    # "mnli",
     # "storycloze_2018"
-    "sst2"
+    # "sst2"
     # "truthfulqa_mc1", --22 and 1
     # "truthfulqa_mc2" --nan
     # "truthfulqa_gen" -- too time taking
