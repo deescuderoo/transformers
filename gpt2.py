adcfdeae57cb0041ae5fe398123930199fb2f16d
adyaagrawal <adyaagrawal04@gmail.com>
2025-04-01 05:42:49 +0530

    running social for gpt medium with piqa tensor

--- gpt2.py
+++ gpt2.py
@@ -31,7 +31,7 @@ from transformers import GPT2LMHeadModelNew
 
 configuration = GPT2Config()
 
-gpt2 = "gpt2-large" # "gpt2-xl" "gpt2" "gpt-large" "gpt2-medium"
+gpt2 = "gpt2-medium" # "gpt2-xl" "gpt2" "gpt-large" "gpt2-medium"
 
 # This is the default GPT2 model from HF
 std_model = GPT2LMHeadModel.from_pretrained(gpt2)
@@ -125,7 +125,7 @@ def newton_inv_sqrt(x):
     '''
     Newton approximation for 1/sqrt(x)
     '''
-    NEWTON_ITERATIONS = 18
+    NEWTON_ITERATIONS = 16
     # Initial estimate
     y = initial_inv_sqrt(x)
     # Iterations
