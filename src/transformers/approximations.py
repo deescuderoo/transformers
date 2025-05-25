c94a11876c045b9ffef01531ebcc82b4df51b241
adyaagrawal <adyaagrawal04@gmail.com>
2025-05-25 13:49:43 +0530

    running gpt-medium piqa with max - piqa, 5, 5, 18

--- src/transformers/approximations.py
+++ src/transformers/approximations.py
@@ -378,7 +378,7 @@ def approx_softmax(x, layer_id, model, dim=None): #FINAL FUNCTION WITH ALL APPRO
     if torch.cuda.is_available():
              maxes = maxes.to('cuda')
 
-    EXP_ITERATIONS = 7
+    EXP_ITERATIONS = 5
     x_diff = (x - maxes).clamp(min=-100, max=100)  # Prevent extreme negatives
     x_exp = approx_exp(x_diff, EXP_ITERATIONS)
     # x_exp = torch.exp(x-maxes)
@@ -402,7 +402,7 @@ def approx_softmax(x, layer_id, model, dim=None): #FINAL FUNCTION WITH ALL APPRO
 
     # norm: divide by length so that quotient is <1 (denominator
     # becomes the mean)
-    G_ITERATIONS = 7
+    G_ITERATIONS = 5
     if torch.cuda.is_available():
         normalizer = normalizer.to('cuda')
         # print(f"Device: {normalizer.device}")
