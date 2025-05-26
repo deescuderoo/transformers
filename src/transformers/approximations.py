0ac1bc136bac3f60caf0521e17d02f6dda3257bf
adyaagrawal <adyaagrawal04@gmail.com>
2025-05-27 03:31:25 +0530

    analysing gpt-medium piqa with max - piqa, 12, 20, 18 * 1.5

--- src/transformers/approximations.py
+++ src/transformers/approximations.py
@@ -368,16 +368,16 @@ def approx_softmax(x, layer_id, model, dim=None): #FINAL FUNCTION WITH ALL APPRO
     maxes = maxes[:, :, :x.shape[2], :] * 1.5
     if torch.cuda.is_available():
              maxes = maxes.to('cuda')
-    EXP_ITERATIONS = 10
+    EXP_ITERATIONS = 12
     x_diff = x - maxes
     x_exp = approx_exp(x_diff, EXP_ITERATIONS)
     x_exp[x <= -3.4028e+37] = 0
     x_exp_sum = torch.sum(x_exp, dim, keepdim=True)
     x_exp_sum = torch.clamp(x_exp_sum, min=1e-12)
     normalizer = torch.ones(x_exp.shape).sum(dim, keepdim=True)
-    G_ITERATIONS = 18
+    G_ITERATIONS = 20
     if torch.cuda.is_available():
         normalizer = normalizer.to('cuda')
     out = approx_div(x_exp / normalizer, x_exp_sum / normalizer,
                      G_ITERATIONS)
-    return out * 0.99 + 0.01 / x.shape[-1]
+    return out
