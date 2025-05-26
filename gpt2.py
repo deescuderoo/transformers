02880c63f0aecf51b5a5e3d20637f6e396e36556
adyaagrawal <adyaagrawal04@gmail.com>
2025-05-26 10:50:49 +0530

    running gpt-medium piqa with max - piqa, 7, 21, 18

--- gpt2.py
+++ gpt2.py
@@ -31,7 +31,7 @@ from transformers import GPT2LMHeadModelNew
 
 configuration = GPT2Config()
 
-gpt2 = "gpt2" # "gpt2-xl" "gpt2" "gpt-large" "gpt2-medium"
+gpt2 = "gpt2-medium" # "gpt2-xl" "gpt2" "gpt-large" "gpt2-medium"
 
 # This is the default GPT2 model from HF
 std_model = GPT2LMHeadModel.from_pretrained(gpt2)
