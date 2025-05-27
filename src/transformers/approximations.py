ac9c329624404a853d24630ece7478860af9c354
adyaagrawal <adyaagrawal04@gmail.com>
2025-05-27 20:14:03 +0530

    analysing gpt-large piqa with max - piqa, 13, 22, 18 * 1.8

--- src/transformers/approximations.py
+++ src/transformers/approximations.py
@@ -365,7 +365,7 @@ def approx_softmax(x, layer_id, model, dim=None): #FINAL FUNCTION WITH ALL APPRO
     tensor_name = f"tensors_{model_name}_{testsuite}"
     tensor_dict = globals().get(tensor_name)
     maxes = tensor_dict[layer_id]
-    maxes = maxes[:, :, :x.shape[2], :] * 1.5
+    maxes = maxes[:, :, :x.shape[2], :] * 1.8
     if torch.cuda.is_available():
              maxes = maxes.to('cuda')
     x_diff = x- maxes
