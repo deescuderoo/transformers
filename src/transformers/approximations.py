de429ed1b28583f07003b2828a9c04e032a90eee
adyaagrawal <adyaagrawal04@gmail.com>
2025-06-03 01:35:23 +0530

    Adding comments

--- src/transformers/approximations.py
+++ src/transformers/approximations.py
@@ -270,7 +270,7 @@ def approx_softmax(x, layer_id, model, dim=None): #FINAL FUNCTION WITH ALL APPRO
     x_exp_sum = torch.sum(x_exp, dim, keepdim=True)
     x_exp_sum = torch.clamp(x_exp_sum, min=1e-12)
     normalizer = torch.ones(x_exp.shape).sum(dim, keepdim=True)
-    G_ITERATIONS = 18
+    G_ITERATIONS = 18 #small = 14, medium = 18, large = 22
     if torch.cuda.is_available():
         normalizer = normalizer.to('cuda')
     out = approx_div(x_exp / normalizer, x_exp_sum / normalizer,
