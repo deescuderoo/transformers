3609be50a36d55a947f759edbf9f3cd7876bfd61
adyaagrawal <adyaagrawal04@gmail.com>
2025-06-05 13:05:28 +0530

    running gpt medium without clamping piqa

--- src/transformers/approximations.py
+++ src/transformers/approximations.py
@@ -268,7 +268,6 @@ def approx_softmax(x, layer_id, model, dim=None): #FINAL FUNCTION WITH ALL APPRO
     x_exp = approx_exp(x_diff, EXP_ITERATIONS)
     x_exp[x <= -3.4028e+37] = 0
     x_exp_sum = torch.sum(x_exp, dim, keepdim=True)
-    x_exp_sum = torch.clamp(x_exp_sum, min=1e-12)
     normalizer = torch.ones(x_exp.shape).sum(dim, keepdim=True)
     G_ITERATIONS = 18 #small = 14, medium = 18, large = 22
     if torch.cuda.is_available():
