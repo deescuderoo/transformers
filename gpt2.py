7fcfcf4de0f66a713b968e75f1c293afbba70398
adyaagrawal <adyaagrawal04@gmail.com>
2025-06-06 20:56:16 +0530

    removing clamp as its not required

--- gpt2.py
+++ gpt2.py
@@ -178,13 +178,13 @@ class NewLayerNorm(nn.Module):
 refln_model = GPT2LMHeadModel.from_pretrained(gpt2)
 gelu_aprxln_model = GPT2LMHeadModel.from_pretrained(gpt2, config=new_config)
 
-# for block in refln_model.transformer.h:
-#    block.ln_1 = RefLayerNorm(block.ln_1)
-#    block.ln_2 = RefLayerNorm(block.ln_2)
-#
-# for block in gelu_aprxln_model.transformer.h:
-#    block.ln_1 = NewLayerNorm(block.ln_1)
-#    block.ln_2 = NewLayerNorm(block.ln_2)
+for block in refln_model.transformer.h:
+   block.ln_1 = RefLayerNorm(block.ln_1)
+   block.ln_2 = RefLayerNorm(block.ln_2)
+
+for block in gelu_aprxln_model.transformer.h:
+   block.ln_1 = NewLayerNorm(block.ln_1)
+   block.ln_2 = NewLayerNorm(block.ln_2)
 
 
 gelu_stdln_aprxsm_model = GPT2LMHeadModelNew.from_pretrained(gpt2, config=new_config)
@@ -233,25 +233,25 @@ std_generated_text = tokenizer.decode(std_output[0], skip_special_tokens=True)
 print("------------------------------------------\n")
 print(f"std output:\n{std_generated_text}")
 
-# gelu_output = gelu_model.generate(input_ids, max_length=100, num_return_sequences=1, pad_token_id=tokenizer.eos_token_id)
-# gelu_generated_text = tokenizer.decode(gelu_output[0], skip_special_tokens=True)
-# print("------------------------------------------\n")
-# print(f"gelu output:\n{gelu_generated_text}")
-#
-# refln_output = refln_model.generate(input_ids, max_length=100, num_return_sequences=1, pad_token_id=tokenizer.eos_token_id)
-# refln_generated_text = tokenizer.decode(refln_output[0], skip_special_tokens=True)
-# print("------------------------------------------\n")
-# print(f"refln output:\n{refln_generated_text}")
-#
-# gelu_aprxln_output = gelu_aprxln_model.generate(input_ids, max_length=100, num_return_sequences=1, pad_token_id=tokenizer.eos_token_id)
-# gelu_aprxln_generated_text = tokenizer.decode(gelu_aprxln_output[0], skip_special_tokens=True)
-# print("------------------------------------------\n")
-# print(f"gelu_aprxln output:\n{gelu_aprxln_generated_text}")
-#
-# gelu_stdln_aprxsm_output = gelu_stdln_aprxsm_model.generate(input_ids, max_length=100, num_return_sequences=1, pad_token_id=tokenizer.eos_token_id)
-# gelu_stdln_aprxsm_generated_text = tokenizer.decode(gelu_stdln_aprxsm_output[0], skip_special_tokens=True)
-# print("------------------------------------------\n")
-# print(f"gelu_stdln_aprxsm output:\n{gelu_stdln_aprxsm_generated_text}")
+gelu_output = gelu_model.generate(input_ids, max_length=100, num_return_sequences=1, pad_token_id=tokenizer.eos_token_id)
+gelu_generated_text = tokenizer.decode(gelu_output[0], skip_special_tokens=True)
+print("------------------------------------------\n")
+print(f"gelu output:\n{gelu_generated_text}")
+
+refln_output = refln_model.generate(input_ids, max_length=100, num_return_sequences=1, pad_token_id=tokenizer.eos_token_id)
+refln_generated_text = tokenizer.decode(refln_output[0], skip_special_tokens=True)
+print("------------------------------------------\n")
+print(f"refln output:\n{refln_generated_text}")
+
+gelu_aprxln_output = gelu_aprxln_model.generate(input_ids, max_length=100, num_return_sequences=1, pad_token_id=tokenizer.eos_token_id)
+gelu_aprxln_generated_text = tokenizer.decode(gelu_aprxln_output[0], skip_special_tokens=True)
+print("------------------------------------------\n")
+print(f"gelu_aprxln output:\n{gelu_aprxln_generated_text}")
+
+gelu_stdln_aprxsm_output = gelu_stdln_aprxsm_model.generate(input_ids, max_length=100, num_return_sequences=1, pad_token_id=tokenizer.eos_token_id)
+gelu_stdln_aprxsm_generated_text = tokenizer.decode(gelu_stdln_aprxsm_output[0], skip_special_tokens=True)
+print("------------------------------------------\n")
+print(f"gelu_stdln_aprxsm output:\n{gelu_stdln_aprxsm_generated_text}")
 
 mod_output = mod_model.generate(input_ids, max_length=100, num_return_sequences=1, pad_token_id=tokenizer.eos_token_id)
 mod_generated_text = tokenizer.decode(mod_output[0], skip_special_tokens=True)
@@ -276,16 +276,16 @@ import lm_eval
 from lm_eval.models.huggingface import HFLM
 # Uncomment the desired tasks
 tasks = [
-    # "hellaswag",
-    # "arc_easy",
-    "piqa"
-    # "social_iqa",
-    # "mnli",
-    # "sst2",
-    # "anli_r1",
-    # "anli_r2",
-    # "anli_r3",
-    # "wic"
+    "hellaswag",
+    "arc_easy",
+    "piqa",
+    "social_iqa",
+    "mnli",
+    "sst2",
+    "anli_r1",
+    "anli_r2",
+    "anli_r3",
+    "wic"
         ]
 batch_size = 8
 task_manager = lm_eval.tasks.TaskManager()
